9bcff63b878399501c2dab1f7aa2f1f9bf1bfe9b
Arthurnacious <arthurnacious@gmail.com>
2024-10-14 14:23:48 +0200

    xata and drizzle

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,8 +1,18 @@
 import PageContainer from "@/components/(app)/page-container";
+import { db } from "@/db";
+import { users } from "@/db/schema";
 import React from "react";
 
-const DahsboardPage = () => {
-  return <PageContainer title="Dashboard">page content</PageContainer>;
+const user = {
+  name: "Arthurnacious Monethi",
+  email: "arthurnacious@gmail.com",
+};
+
+const DahsboardPage = async () => {
+  const allUsers = await db.select().from(users);
+  return (
+    <PageContainer title="Dashboard">{allUsers.length} users</PageContainer>
+  );
 };
 
 export default DahsboardPage;
